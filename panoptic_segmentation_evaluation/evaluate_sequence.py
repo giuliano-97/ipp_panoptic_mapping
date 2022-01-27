922fbc83ba60e9ba61ceb7160033517fa2b7346e
Giuliano Albanese <albanese.giuliano97@gmail.com>
2022-01-27 01:35:55 +0100

    Implement cumulative 2d metrics evaluation

--- panoptic_segmentation_evaluation/evaluate_sequence.py
+++ panoptic_segmentation_evaluation/evaluate_sequence.py
@@ -22,9 +22,10 @@ def _encode_panoptic(label_image: np.ndarray):
     return label_image[:, :, 0] * _MAX_INSTANCES_PER_LABEL + label_image[:, :, 1]
 
 
-def eval_panoptic_quality(
+def evaluate_sequence(
     pred_labels_dir: str,
     gt_labels_dir: str,
+    metrics_log_interval: int,
     output_dir: str,
 ):
     assert os.path.isdir(pred_labels_dir), f"{pred_labels_dir} is not a valid dir."
@@ -39,10 +40,7 @@ def eval_panoptic_quality(
     )
 
     pred_labels_files = glob.glob(f"{pred_labels_dir}/*.png")
-    metrics_data = np.empty(
-        shape=(len(pred_labels_files), len(_METRICS_NAMES)),
-        dtype=np.float32,
-    )
+    metrics_data = []
     for i, pred_labels_file in enumerate(sorted(pred_labels_files)):
         gt_labels_file = os.path.join(gt_labels_dir, os.path.basename(pred_labels_file))
         assert os.path.exists(
@@ -59,20 +57,18 @@ def eval_panoptic_quality(
             tf.convert_to_tensor(pred_labels),
         )
 
-        metrics_data[i, :] = pq.result().numpy()
+        if i % metrics_log_interval == 0 or i == len(pred_labels_files) - 1:
+            metrics_data_entry = dict(zip(_METRICS_NAMES, pq.result().numpy().tolist()))
+            metrics_data_entry["FrameID"] = i + 1
+            metrics_data.append(metrics_data_entry)
 
-        pq.reset_states()
-
-    metrics_df = pd.DataFrame(
-        metrics_data,
-        columns=_METRICS_NAMES,
-    )
+    metrics_df = pd.DataFrame(metrics_data)
     metrics_file_path = os.path.join(
         output_dir,
-        os.path.basename(pred_labels_dir) + "_metrics.csv",
+        "panoptic_segmentation_metrics.csv",
     )
 
-    metrics_df.to_csv(metrics_file_path, index=False)
+    metrics_df.set_index("FrameID").to_csv(metrics_file_path)
 
 
 def _parse_args():
@@ -81,15 +77,19 @@ def _parse_args():
     )
 
     parser.add_argument(
-        "--pred_labels_dir",
+        "pred_labels_dir",
         type=str,
-        required=True,
     )
 
     parser.add_argument(
-        "--gt_labels_dir",
+        "gt_labels_dir",
         type=str,
-        required=True,
+    )
+    parser.add_argument(
+        "--metrics_log_interval",
+        type=int,
+        default=5,
+        help="Save partial results every n frames.",
     )
 
     parser.add_argument(
@@ -103,8 +103,9 @@ def _parse_args():
 
 if __name__ == "__main__":
     args = _parse_args()
-    eval_panoptic_quality(
+    evaluate_sequence(
         args.pred_labels_dir,
         args.gt_labels_dir,
+        args.metrics_log_interval,
         args.output_dir,
     )
