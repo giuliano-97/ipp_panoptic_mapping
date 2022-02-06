fc54a43c5ed1ff2adbfef452c5fb696f102a3df2
Giuliano Albanese <albanese.giuliano97@gmail.com>
2022-02-06 18:59:47 +0100

    Add script to create voxel segs for all pcd in dir

---
+++ panoptic_mapping_evaluation/create_voxel_segs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+
+if [ ! -d $1 ]
+then
+    echo "Usage create_voxel_segs.sh <PANOPTIC_CLOUDS_DIR_PATH>"
+    exit 1
+fi
+
+CLOUD_TO_SEGS=$(dirname $(dirname "${BASH_SOURCE[0]}"))/build/panoptic_mapping_evaluation/cloud_to_segs
+
+PANOPTIC_CLOUDS_DIR_PATH=$1
+PANOPTIC_CLOUD_FILES=$(find ${PANOPTIC_CLOUDS_DIR_PATH} -name \*.pointcloud.ply)
+
+for FILE in $PANOPTIC_CLOUD_FILES
+do
+    echo "Processing file $(basename $FILE)..."
+    $CLOUD_TO_SEGS $FILE
+    echo "Done."
+done
