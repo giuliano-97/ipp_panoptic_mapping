2b7af76339cbf4f2edb4159234a491cda66b097b
Giuliano Albanese <albanese.giuliano97@gmail.com>
2022-02-06 21:34:38 +0100

    Fail create_voxel_segs if cloud_to_segs not found

--- panoptic_mapping_evaluation/create_voxel_segs.sh
+++ panoptic_mapping_evaluation/create_voxel_segs.sh
@@ -8,6 +8,11 @@ then
 fi
 
 CLOUD_TO_SEGS=$(dirname $(dirname "${BASH_SOURCE[0]}"))/build/panoptic_mapping_evaluation/cloud_to_segs
+if [ ! -f $CLOUD_TO_SEGS ]
+then
+    echo "cloud_to_segs binary not found - did you build the code correctly?"
+    exit 1
+fi
 
 PANOPTIC_CLOUDS_DIR_PATH=$1
 PANOPTIC_CLOUD_FILES=$(find ${PANOPTIC_CLOUDS_DIR_PATH} -name \*.pointcloud.ply)
