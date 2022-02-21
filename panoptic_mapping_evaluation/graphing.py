8861c6a0f69ed9ead5d93cdea1bd660117b86e2c
Giuliano Albanese <albanese.giuliano97@gmail.com>
2022-02-21 21:48:02 +0100

    Clear figure after plot

--- panoptic_mapping_evaluation/graphing.py
+++ panoptic_mapping_evaluation/graphing.py
@@ -10,12 +10,13 @@ import seaborn as sns
 # Turn off interactive mode
 plt.ioff()
 
-
 def save_grouped_barplot(
     metrics_df: pd.DataFrame,
     barplot_file_path: Path,
     pivot_column: str = "method",
 ):
+    plt.figure(figsize=(15,8))
+
     # Convert metrics dataframe from wide to long format
     metrics_df_long = pd.melt(metrics_df, id_vars=[pivot_column], var_name="metric")
     # Create catplot
@@ -25,14 +26,19 @@ def save_grouped_barplot(
         x=pivot_column,
         y="value",
         hue="metric",
+        height=8,
+        aspect=15 / 8,
     )
 
     # Configure plot
     grid.legend.set_title("Metrics")
 
     # Save the plot
-
     grid.figure.savefig(str(barplot_file_path))
+
+    # Clean up
+    grid.figure.clear()
+    plt.close(grid.figure)
 
 def save_trend_lineplot(
     metrics_df: pd.DataFrame,
@@ -49,3 +55,7 @@ def save_trend_lineplot(
     )    
 
     ax.figure.savefig(str(lineplot_file_path))
+    
+    # Clean up
+    ax.figure.clear()
+    plt.close(ax.figure)
