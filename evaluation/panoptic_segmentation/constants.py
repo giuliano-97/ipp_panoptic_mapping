91e08e3aa8ada08e51b876fd139db26c1166f1e3
Giuliano Albanese <albanese.giuliano97@gmail.com>
2022-02-22 15:41:24 +0100

    Add constants for pano seg evaluation

---
+++ evaluation/panoptic_segmentation/constants.py
@@ -0,0 +1,12 @@
+from evaluation.panoptic_mapping.constants import PRQ_SRQ_RRQ_KEYS, TP_FP_FN_KEYS
+
+
+PQ_KEY = "PQ"
+SQ_KEY = "SQ"
+RQ_KEY = "RQ"
+TP_KEY = "TP"
+FP_KEY = "FP"
+FN_KEY = "FN"
+
+PQ_SQ_RQ_KEYS = [PQ_KEY, SQ_KEY, RQ_KEY]
+TP_FN_FP_KEYS = [TP_KEY, FN_KEY, FP_KEY]
